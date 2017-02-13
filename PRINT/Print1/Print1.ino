0ff14c09a1b6b5fa9eb055ec98b9f47142b9906a
Gorkabmc <gorkabmc@gmail.com>
2017-02-13 21:05:28 +0100

    print1

--- PRINT/Print1/Print1.ino
+++ PRINT/Print1/Print1.ino
@@ -8,11 +8,27 @@
 //******************************* INCLUDES ****************************************
 
 //******************************* VARIABLES ***************************************
-
+int num = 64;
 //******************************** SETUP ******************************************
 void setup()
-{
-}
+  {
+  Serial.begin(9600);    // set up Serial library at 9600 bps
+
+  Serial.println("Different formats for the same number:");
+
+  Serial.write(num);
+  Serial.println();
+  
+  
+  Serial.println(num,DEC);
+  
+  Serial.println(num,BIN);
+  
+  Serial.println(num,HEX);
+  
+  Serial.println(num,OCT);
+
+  }
 
 //********************************* LOOP ******************************************
 void loop()
