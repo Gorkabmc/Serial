682d89ac397c79d1ebed5fa900b159c286929317
Gorkabmc <gorkabmc@gmail.com>
2017-02-13 20:48:08 +0100

    9

--- MATH/_9_MATH_IS_FUN/_9_MATH_IS_FUN.ino
+++ MATH/_9_MATH_IS_FUN/_9_MATH_IS_FUN.ino
@@ -8,15 +8,31 @@
 //******************************* INCLUDES ****************************************
 
 //******************************* VARIABLES ***************************************
-
+  int a = 3;
+  int b = 2;
+  int d;
 //******************************** SETUP ******************************************
-void setup()
-{
-}
+  void setup()
+  {
+  Serial.begin(9600);      // set up Serial library at 9600 bps
+
+  Serial.println("Here is division: ");
+
+  Serial.print("a = ");
+  Serial.println(a);
+  Serial.print("b = ");
+  Serial.println(b);
+    
+  d = a / b;
+ 
+  Serial.print("a / b = ");
+  Serial.println(d);
+
+  }
 
 //********************************* LOOP ******************************************
-void loop()
-{
-}
+  void loop()
+  {
+  }
 
 //******************************* FUNCIONS ****************************************
