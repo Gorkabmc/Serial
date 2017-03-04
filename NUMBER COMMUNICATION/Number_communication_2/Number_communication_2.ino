b0b353bb82dd02f0f822d612d0c29503b42066fa
Gorkabmc <gorkabmc@gmail.com>
2017-03-04 16:12:09 +0100

    number2

---
+++ NUMBER COMMUNICATION/Number_communication_2/Number_communication_2.ino
@@ -0,0 +1,37 @@
+/**********************************************************************************
+**                                                                               **
+**                          PLANTILLA CFGS                                       **
+**                                                                               **
+** GORKA FERNÁNDEZ RAMOS                                       4/03/2017        **
+**********************************************************************************/
+
+//******************************* INCLUDES ****************************************
+
+//******************************* VARIABLES ***************************************
+int n1;
+//******************************** SETUP ******************************************
+void setup()
+{
+  Serial.begin(9600);        // initialize serial
+  Serial.println("Entrar numero");
+}
+
+//********************************* LOOP ******************************************
+void loop()
+{
+  while (Serial.available() > 0) {  // if there's any serial available, read it
+    r1 = Serial.parseInt();// look for valid int the incoming serial stream
+    Serial.print("Numero");
+    Serial.print(n1);
+   
+    if (n1%2==0') {
+      Serial.print ("parell");
+  }
+else 
+{Serial.print ( senar )
+if 
+Serial.read () =='\n')
+Serial.print ( "Entrar un altre numero" )
+  }
+}
+//******************************* FUNCIONS ****************************************
