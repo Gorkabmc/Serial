2937920c43e2f40af6286695b21f97465d3a0b08
Gorkabmc <gorkabmc@gmail.com>
2017-03-04 15:37:44 +0100

    for2

--- CONTROL STRUCTURES/for_2/for_2.ino
+++ CONTROL STRUCTURES/for_2/for_2.ino
@@ -12,19 +12,20 @@ int comptar = 11;
 //******************************** SETUP ******************************************
 void setup()
 {
-    Serial.begin(9600);       // set up Serial library at 9600 bps
+  Serial.begin(9600);       // set up Serial library at 9600 bps
   Serial.print("Ara comptare de 0 a ");
   Serial.println(comptar);
-  for (int i=0; i <= comptar; i++)
+  for (int i = 0; i <= comptar; i++)
   {
-    if (i<11)
-  }
-    Serial.print(i);
-    Serial.print("-");
+    if (i < 11)
+    }
+  Serial.print(i);
+  Serial.print("-");
 }
-else if (i>=11)
+else if (i >= 11)
 }
-  } 
+serial.print(i);
+}
 }
 
 //********************************* LOOP ******************************************
