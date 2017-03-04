5f1ad506bfd3f7635c0ad112a7d1ff1ab336a2aa
Gorkabmc <gorkabmc@gmail.com>
2017-03-04 16:02:11 +0100

    number1

--- NUMBER COMMUNICATION/Number_Communication_1/Number_Communication_1.ino
+++ NUMBER COMMUNICATION/Number_Communication_1/Number_Communication_1.ino
@@ -8,38 +8,40 @@
 //******************************* INCLUDES ****************************************
 
 //******************************* VARIABLES ***************************************
-float r1, r2;             
-float rSerie, rParalel;   
+float r1, r2;
+float rSerie, rParalel;
 //******************************** SETUP ******************************************
 void setup()
 {
-    Serial.begin(9600);        // initialize serial
+  Serial.begin(9600);        // initialize serial
   Serial.println("Entra el valor de r1 i r2 (separats per una coma)");
 }
 
 //********************************* LOOP ******************************************
 void loop()
 {
-    while (Serial.available() > 0) {  // if there's any serial available, read it
+  while (Serial.available() > 0) {  // if there's any serial available, read it
     r1 = Serial.parseInt();// look for valid int the incoming serial stream
     Serial.print("r1=");
     Serial.print(r1);
     Serial.print("ohms");
-   
+
     r2 = Serial.parseInt();
-      Serial.print ("r2 = ");       
-      Serial.print (r2);            
-      Serial.println ("Ohms");
-   
-    if (Serial.read() == '\n') { 
-      Serial.print ("rSerie = ");   
-      Serial.print (r1+r2);        
+    Serial.print ("r2 = ");
+    Serial.print (r2);
+    Serial.println ("Ohms");
+
+    if (Serial.read() == '\n') {
+      Serial.print ("rSerie = ");
+      Serial.print (r1 + r2);
       Serial.print ("ohms");
 
       Serial.print ("rParalel = "); // Escriu (rParalel = )
-      Serial.print ((r1*r2)/(r1+r2));
-      Serial.println ("Ohms"); 
+      Serial.print ((r1 * r2) / (r1 + r2));
+      Serial.println ("Ohms");
+      Serial.println (" ");
+      Serial.println ("Entra nous valors per r1 i r2");
     }
-}
+  }
 }
 //******************************* FUNCIONS ****************************************
