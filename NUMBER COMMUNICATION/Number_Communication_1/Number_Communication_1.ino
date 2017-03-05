c588c45f1ad7afd98893dac1f4a724e944fcc5d5
Gorkabmc <gorkabmc@gmail.com>
2017-03-05 17:04:23 +0100

    NUMBER COMMUNICATION

--- NUMBER COMMUNICATION/Number_Communication_1/Number_Communication_1.ino
+++ NUMBER COMMUNICATION/Number_Communication_1/Number_Communication_1.ino
@@ -1,35 +1,37 @@
 /**********************************************************************************
 **                                                                               **
-**                          PLANTILLA CFGS                                       **
+**                          NUMBER COMMUNICATION 1                               **
 **                                                                               **
-** GORKA FERNÁNDEZ RAMOS                                       4/03/2017        **
+**   GORKA FERNÁNDEZ RAMOS                                         4/03/2017     **
 **********************************************************************************/
 
 //******************************* INCLUDES ****************************************
 
 //******************************* VARIABLES ***************************************
 float r1, r2;
-float rSerie, rParalel;
+float rSerie, rParalel; // VALOR DE LES VARIABLES
 //******************************** SETUP ******************************************
-void setup()
+void setup() // S'EXECUTA NOMÉS UN COP
 {
-  Serial.begin(9600);        // initialize serial
+  Serial.begin(9600);        // VELOCICTAT DEL PORT SERIE
   Serial.println("Entra el valor de r1 i r2 (separats per una coma)");
+  // IMPRIMEIX entra el valor de r1 ...
+
 }
 
 //********************************* LOOP ******************************************
-void loop()
+void loop() // S'EXECUTA REPETIDAMENT
 {
-  while (Serial.available() > 0) {  // if there's any serial available, read it
-    r1 = Serial.parseInt();// look for valid int the incoming serial stream
-    Serial.print("r1=");
-    Serial.print(r1);
-    Serial.print("  ohms  ");
+  while (Serial.available() > 0) {  // LLEGEIX LA RSERIE
+    r1 = Serial.parseInt();//
+    Serial.print("r1="); // ESCRIU r1 =
+    Serial.print(r1); // ESCRIU EL VALOR DE LA r1
+    Serial.print("  ohms  "); // ESCRIU ohms
 
     r2 = Serial.parseInt();
-    Serial.print ("r2 = ");
-    Serial.print (r2);
-    Serial.println ("  Ohms  ");
+    Serial.print ("r2 = "); // ESCRIU r2 = 
+    Serial.print (r2); // ESCRIU EL VALOR DE r2
+    Serial.println ("  Ohms  "); // ESCRIU ohms
 
     if (Serial.read() == '\n') {
       Serial.print ("rSerie = ");
@@ -37,10 +39,10 @@ void loop()
       Serial.print ("  ohms  ");
 
       Serial.print ("rParalel = "); // Escriu (rParalel = )
-      Serial.print ((r1 * r2) / (r1 + r2));
-      Serial.println ("  Ohms  ");
-      Serial.println (" ");
-      Serial.println ("Entra nous valors per r1 i r2");
+      Serial.print ((r1 * r2) / (r1 + r2)); // ESCRIU EL VALOR DE DIVIDIR ELS ()
+      Serial.println ("  Ohms  "); // ESCRIU ohms 
+      Serial.println (" "); // PASA A SEGUEN LINEA 
+      Serial.println ("Entra nous valors per r1 i r2"); // ESCRIU entra nous...
     }
   }
 }
--- NUMBER COMMUNICATION/Number_communication_2/Number_communication_2.ino
+++ NUMBER COMMUNICATION/Number_communication_2/Number_communication_2.ino
@@ -1,41 +1,44 @@
 /**********************************************************************************
 **                                                                               **
-**                          PLANTILLA CFGS                                       **
+**                          NUMBER COMMUNICATION 2                               **
 **                                                                               **
-** GORKA FERNÁNDEZ RAMOS                                       4/03/2017        **
+**    GORKA FERNÁNDEZ RAMOS                                         4/03/2017    **
 **********************************************************************************/
 
 //******************************* INCLUDES ****************************************
 
 //******************************* VARIABLES ***************************************
-int n1;
+int n1; // VALOR DE VARIABLE 
 //******************************** SETUP ******************************************
-void setup()
+void setup() // S'EXECUTA UN COP 
 {
-  Serial.begin(9600);        // initialize serial
-  Serial.println("Entrar numero");
+  Serial.begin(9600);        // VELOCITAT DE ARRANC A 9600BPM
+  Serial.println("Entrar numero"); // IMPRIMEIX entrar numero..
 }
 
 //********************************* LOOP ******************************************
-void loop()
+void loop() // S'EXECUTA REPETIDAMENT 
 {
-  while (Serial.available() > 0) {  // if there's any serial available, read it
-    n1 = Serial.parseFloat();// look for valid int the incoming serial stream
-    Serial.print(" Numero ");
-    Serial.print(n1);
+  while (Serial.available() > 0) {  // LLEGEIX ELS NUMEROS DEL SERIAL
+    n1 = Serial.parseFloat();// 
+    Serial.print(" Numero "); // IMPRIMEIX numero 
+    Serial.print(n1); // IMPRIMEIX EL VALOR DE n1
 
     if (n1 % 2 == 0 )
     {
-      Serial.println (" <- parell ");
+      Serial.println (" <- parell "); 
+      // SI EL MODUL DE n1 DIVIDIT ENTRE 2 ES = 0 IMPRIMEIX parell
     }
     else
     {
       Serial.println ( " <- senar " );
+      // SI EL MODUL DE n1 no es = 0 IMPRIMEIX senar
     }
     if
     (Serial.read () == '\n')
-      Serial.println ( "  " );
+      Serial.println ( "  " ); // IMPRIMEIX UN ( _ )
     Serial.println ( " Entrar un altre numero " );
+    // IMPRIMEIX entrar un altre numero
   }
 }
 //******************************* FUNCIONS ****************************************
